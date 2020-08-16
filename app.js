640ddf5f19fa7a3f42971b555500b1b6ed51a924
shovancak <s.hovancak@gmail.com>
2020-08-16 16:01:38 +0200

    Creating error handling middleware

--- app.js
+++ app.js
@@ -6,6 +6,18 @@ const PORT = 5000;
 
 app.use("/api/articles", articleRoutes);
 
+// default error handling middleware provided by Express
+// executes when any middleware before has an error
+app.use((error, req, res, next) => {
+  //if response has been sent, return an error
+  if (res.headerSent) {
+    return next(error);
+  }
+  res
+    .status(error.code || 500)
+    .json({ message: error.message || "An unknown error occurred!" });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is runnig at port: ${PORT}`);
 });
