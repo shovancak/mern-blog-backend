a4e2a3d1d27660dd639c715ec3ec8076070032b4
shovancak <s.hovancak@gmail.com>
2020-08-18 19:58:07 +0200

    Adding error handling middleware for unsupported routes.

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const articleRoutes = require("./routes/article-routes");
+const HttpError = require("./models/http-error-model");
 const app = express();
 const PORT = 5000;
 
 app.use(bodyParser.json());
 
 app.use("/api/articles", articleRoutes);
+
+// middleware handling ONLY routes, which have not been handled before
+// reached ONLY if we have some request, which didnt get response before
+app.use((req, res, next) => {
+  const error = new HttpError("Could not find this route.", 404);
+  throw error;
+});
 
 // default error handling middleware provided by Express
 // executes when any middleware before has an error
