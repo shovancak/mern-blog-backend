6edad2899beee5a39b72470cc5a9c2b1c6a5653f
shovancak <s.hovancak@gmail.com>
2020-08-21 15:08:28 +0200

    Adding route for getting list of users

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,2 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const HttpError = require("../models/http-error-model");
+const userControllers = require("../controllers/user-controllers");
+
+router.get("/", userControllers.getListOfAllUsers);
+
+module.exports = router;
