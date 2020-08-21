ca5de3893a1c1a2b571551dabd198cca0d778f5c
shovancak <s.hovancak@gmail.com>
2020-08-21 17:26:08 +0200

    Adding post route for singing up new user

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const HttpError = require("../models/http-error-model");
 const userControllers = require("../controllers/user-controllers");
 
+// Getting list of all users
 router.get("/", userControllers.getListOfAllUsers);
+// Signing up new user
+router.post("/signup", userControllers.signupUser);
 
 module.exports = router;
